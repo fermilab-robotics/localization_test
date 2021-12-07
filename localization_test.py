ac11766390dc260d2a01efe383c16ba5a1466ef3
ncurfman <94872870+ncurfman@users.noreply.github.com>
2021-12-07 12:58:19 -0600

    Update localization_test.py

--- localization_test.py
+++ localization_test.py
@@ -1,12 +1,3 @@
-#Noah Curfman
-#11/18/2021
-
-
-""" 
-Printout and log to .csv Spot vision frame, Spot odom frame, and
-frames of all fiducials in range.
-"""
-
 from __future__ import print_function
 import argparse
 import csv
@@ -34,23 +25,17 @@ def is_docker():
 
 
 def create_csv_filename():
-    if is_docker() and containerDebuginVS:
+    if is_docker() or containerDebuginVS:
         return f'/pos_out/localization_test_output_{time.strftime("%Y%m%d-%H%M%S")}.csv'
     return f'localization_test_output_{time.strftime("%Y%m%d-%H%M%S")}.csv'
 
 
 def main(argv):
 
-
     parser = argparse.ArgumentParser()
     bosdyn.client.util.add_common_arguments(parser)
-
-    #If running in a container env in VSCode use -vs to be able to run this script from the VScode terminal with
-    #python3 localization_test.py -vs --username $USERNAME --password $PASSWORD $ROBOT_IP
     parser.add_argument('-vs', dest='containerDebuginVS', action='store_true')
     options = parser.parse_args(argv)
-
- 
 
     # Create robot object with a world object client.
     sdk = bosdyn.client.create_standard_sdk('localization_test_client ')
@@ -71,18 +56,15 @@ def main(argv):
         while True:
             input("Press Enter to log robot and fiducial positions...")
 
-
             # Get info for vision and odom frames 
             odom_T_body = get_a_tform_b(robot_state_client.get_robot_state().kinematic_state.transforms_snapshot,"odom","body")
             vision_T_body = get_a_tform_b(robot_state_client.get_robot_state().kinematic_state.transforms_snapshot,"vision","body") 
             robotPositionTime = datetime.datetime.now()
 
-
             # Get all fiducial objects (an object of a specific type).
             request_fiducials = [world_object_pb2.WORLD_OBJECT_APRILTAG]
             fiducial_objects = world_object_client.list_world_objects(object_type=request_fiducials).world_objects
             fiducialTime = datetime.datetime.now()
-
 
             #Convert Quaternions to Euler Angles
             odomBodyEuler = quat_to_eulerZYX(odom_T_body.rot)
@@ -103,7 +85,6 @@ def main(argv):
             print(f'    roll:{odomBodyEuler[2]}')
             print("")
 
-
             print("Vision:")
             print("    x: " + str(vision_T_body.x))
             print("    y: " + str(vision_T_body.y))
@@ -111,7 +92,6 @@ def main(argv):
             print(f'    yaw:{visionBodyEuler[0]}')
             print(f'    pitch:{visionBodyEuler[1]}')
             print(f'    roll:{visionBodyEuler[2]}')
-
             
             # Log robot position data
             data_entry = {
@@ -131,7 +111,6 @@ def main(argv):
             }
 
             data_list.append(data_entry)
-           
             
             # Fiducial position data header printout            
             print("")
@@ -139,14 +118,12 @@ def main(argv):
             print("time: " + str(fiducialTime))
             print()
 
-
             for fiducial in fiducial_objects:
                 vision_T_fiducial = get_a_tform_b(fiducial.transforms_snapshot, VISION_FRAME_NAME, fiducial.apriltag_properties.frame_name_fiducial)
                 odom_T_fiducial = get_a_tform_b(fiducial.transforms_snapshot, ODOM_FRAME_NAME, fiducial.apriltag_properties.frame_name_fiducial)
 
                 odomTagEuler = quat_to_eulerZYX(odom_T_fiducial.rot)
                 visionTagEuler = quat_to_eulerZYX(vision_T_fiducial.rot)
-
 
                 # Fidcucial position data printout
                 print("Odom " + fiducial.name + ":")
@@ -158,7 +135,6 @@ def main(argv):
                 print(f'    roll:{odomTagEuler[2]}')
                 print("")
 
-
                 print("Vision " + fiducial.name +  ":")
                 print("    x: "+ str(vision_T_fiducial.x))
                 print("    y: "+ str(vision_T_fiducial.y))
@@ -167,7 +143,6 @@ def main(argv):
                 print(f'    pitch:{visionTagEuler[1]}')
                 print(f'    roll:{visionTagEuler[2]}')
                 print('') 
-
 
                 #Log fiducial position data
                 data_entry.update({
@@ -185,12 +160,9 @@ def main(argv):
                   fiducial.name + '_vis_roll': visionTagEuler[2],
                 })
          
-
             data_entry.update({'tag_time' : fiducialTime})
             data_list.append(data_entry)
                                
-       
-
             # Create csv with header then write latest row
             keys = data_list[0].keys()
             if curr_fname == '':
@@ -202,12 +174,9 @@ def main(argv):
                 dict_writer = csv.DictWriter(output_file, keys)
                 dict_writer.writerow(data_list[-1])
 
-
     except KeyboardInterrupt:
         print("Caught KeyboardInterrupt, exiting")
         return True
- 
-
 
 if __name__ == '__main__':
     if not main(sys.argv[1:]):
